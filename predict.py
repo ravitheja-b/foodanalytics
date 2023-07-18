e9eb34f9109775aa89e40b7a14927527de2aca9c
ravitheja-b <60288647+ravitheja-b@users.noreply.github.com>
2023-07-18 17:15:50 +0530

    Add files via upload

--- predict.py
+++ predict.py
@@ -25,8 +25,8 @@ data.replace({'event':{'breakfast':1,'lunch':2,'snacks':3}},inplace=True)
 data.replace({'menu':{'idli':1, 'boiled egg':2, 'Chicken curry':3, 'veg biriyani':4, 'Chappati':5,'Veg meals':6, 'Kheer':7, 'Samosa':8, 'rawa idli':9, 'Jeera rice':10,'Rosgulla':11, 'Curry bun':12, 'poori':13, ' Chicken Biryani':14, 'Veg pulav':15,'Gulab jamun':16, 'egg Puff':17, 'Puff':18, 'Vada':19, 'Puliogere':20,'Fruit salad':21, 'thatte idli':22, 'Chicken biriyani':23, 'Veg fried rice':24,'masala dosa':25}},inplace=True) 
 data.replace({'diet':{'Vegeterian':1,'Non-Vegeterian':2}},inplace=True) 
 
-X = data.drop(['date','menu-id','flavor_profile','calorielevel-per-100gm','wastage%'],axis=1)
-Y = data['wastage%'] 
+X = data.drop(['date','menu-id','flavor_profile','calorielevel-per-100gm','consumed-qty'],axis=1)
+Y = data['consumed-qty'] 
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state=42)
 lin_reg_model = LinearRegression()
@@ -77,8 +77,14 @@ fig = sns.regplot(x=Y_test, y=Y_pred, scatter_kws={"color": "green"}, line_kws={
 plt.savefig('predict-image.png')
 
 idli=data[(data["day"] == 1) & (data["menu"] == 1) ]
-samosa = data[(data['day'] == 1) & (data['menu'] == 8)]
+boiledegg = data[(data["day"] == 1) & (data["menu"] == 2) ]
+Chicken_curry = data[(data["day"] == 1) & (data["menu"] == 3) ]
+veg_biriyani = data[(data["day"] == 1) & (data["menu"] == 4) ]
 chap=data[(data["day"] == 1) & (data["menu"] == 5) ]
+veg_meals = data[(data["day"] == 1) & (data["menu"] == 6) ]
+desert= data[(data["day"] == 1) & (data["menu"] == 7) ]
+samosa = data[(data['day'] == 1) & (data['menu'] == 8)]
+
 
 def idli_wastage_pred():
     Xidli = idli.drop(['date', 'menu-id', 'flavor_profile',
@@ -99,6 +105,9 @@ def idli_wastage_pred():
     print ("<br/><b>Predicted Idli wastage :", Yidli_pred)
     
     
+
+
+
 def samosa_wastage_pred():
     Xsamosa = samosa.drop(['date', 'menu-id', 'flavor_profile',
                           'calorielevel-per-100gm', 'wastage%'], axis=1)
@@ -119,6 +128,7 @@ def samosa_wastage_pred():
     print ("<br/><b>Predicted Samosa wastage :", Ysamosa_pred)
     
 
+
 def roti_wastage_pred():
     Xchap = chap.drop(['date', 'menu-id', 'flavor_profile',
                       'calorielevel-per-100gm', 'wastage%'], axis=1)
@@ -136,8 +146,105 @@ def roti_wastage_pred():
     test_error_score = metrics.r2_score(Ychap_test, Ychap_pred)
    
     print ("<br/><b>Predicted Roti wastage :", Ychap_pred)
+    
 
+def boiledegg_wastage_pred():
+    Xboiledegg = boiledegg.drop(['date', 'menu-id', 'flavor_profile',
+                      'calorielevel-per-100gm', 'wastage%'], axis=1)
+    Yboiledegg = boiledegg['wastage%']
+
+    (Xboiledegg_train, Xboiledegg_test, Yboiledegg_train, Yboiledegg_test) = \
+        train_test_split(Xboiledegg, Yboiledegg, test_size=0.2, random_state=42)
+    lin_reg_model = LinearRegression()
+    lin_reg_model.fit(Xboiledegg_train, Yboiledegg_train)
+    training_boiledegg_prediction = lin_reg_model.predict(Xboiledegg_train)
+    train_error_score = metrics.r2_score(Yboiledegg_train,
+            training_boiledegg_prediction)
+    Yboiledegg_pred = lin_reg_model.predict(Xboiledegg_test)
+    test_error_score = metrics.r2_score(Yboiledegg_test, Yboiledegg_pred)
+    print ("<br/><b>Predicted boiledegg wastage :", Yboiledegg_pred)
+
+    
+
+
+def Chicken_curry_wastage_pred():
+    XChicken_curry = Chicken_curry.drop(['date', 'menu-id', 'flavor_profile',
+                      'calorielevel-per-100gm', 'wastage%'], axis=1)
+    YChicken_curry = Chicken_curry['wastage%']
+
+    (XChicken_curry_train, XChicken_curry_test, YChicken_curry_train, YChicken_curry_test) = \
+        train_test_split(XChicken_curry, YChicken_curry, test_size=0.2, random_state=42)
+    lin_reg_model = LinearRegression()
+    lin_reg_model.fit(XChicken_curry_train, YChicken_curry_train)
+    training_Chicken_curry_prediction = lin_reg_model.predict(XChicken_curry_train)
+    train_error_score = metrics.r2_score(YChicken_curry_train,
+            training_Chicken_curry_prediction)
+    YChicken_curry_pred = lin_reg_model.predict(XChicken_curry_test)
+    test_error_score = metrics.r2_score(YChicken_curry_test, YChicken_curry_pred)
+    print ("<br/><b>Predicted Chicken curry wastage :", YChicken_curry_pred)
+
+
+def veg_biriyani_wastage_pred():
+    Xveg_biriyani = veg_biriyani.drop(['date', 'menu-id', 'flavor_profile',
+                      'calorielevel-per-100gm', 'wastage%'], axis=1)
+    Yveg_biriyani = veg_biriyani['wastage%']
+
+    (Xveg_biriyani_train, Xveg_biriyani_test, Yveg_biriyani_train, Yveg_biriyani_test) = \
+        train_test_split(Xveg_biriyani, Yveg_biriyani, test_size=0.2, random_state=42)
+    lin_reg_model = LinearRegression()
+    lin_reg_model.fit(Xveg_biriyani_train, Yveg_biriyani_train)
+    training_veg_biriyani_prediction = lin_reg_model.predict(Xveg_biriyani_train)
+    train_error_score = metrics.r2_score(Yveg_biriyani_train,
+            training_veg_biriyani_prediction)
+    Yveg_biriyani_pred = lin_reg_model.predict(Xveg_biriyani_test)
+    test_error_score = metrics.r2_score(Yveg_biriyani_test, Yveg_biriyani_pred)
+    print ("<br/><b>Predicted Veg biryani wastage :", Yveg_biriyani_pred)
+
+
+def veg_meals_wastage_pred():
+    Xveg_meals = veg_meals.drop(['date', 'menu-id', 'flavor_profile',
+                      'calorielevel-per-100gm', 'wastage%'], axis=1)
+    Yveg_meals = veg_meals['wastage%']
+
+    (Xveg_meals_train, Xveg_meals_test, Yveg_meals_train, Yveg_meals_test) = \
+        train_test_split(Xveg_meals, Yveg_meals, test_size=0.2, random_state=42)
+    lin_reg_model = LinearRegression()
+    lin_reg_model.fit(Xveg_meals_train, Yveg_meals_train)
+    training_veg_meals_prediction = lin_reg_model.predict(Xveg_meals_train)
+    train_error_score = metrics.r2_score(Yveg_meals_train,
+            training_veg_meals_prediction)
+    Yveg_meals_pred = lin_reg_model.predict(Xveg_meals_test)
+    test_error_score = metrics.r2_score(Yveg_meals_test, Yveg_meals_pred)
+    print ("<br/><b>Predicted Veg meals wastage :", Yveg_meals_pred)
+    
+
+
+def desert_wastage_pred():
+    Xdesert = desert.drop(['date', 'menu-id', 'flavor_profile',
+                      'calorielevel-per-100gm', 'wastage%'], axis=1)
+    Ydesert = desert['wastage%']
+
+    (Xdesert_train, Xdesert_test, Ydesert_train, Ydesert_test) = \
+        train_test_split(Xdesert, Ydesert, test_size=0.2, random_state=42)
+    lin_reg_model = LinearRegression()
+    lin_reg_model.fit(Xdesert_train, Ydesert_train)
+    training_desert_prediction = lin_reg_model.predict(Xdesert_train)
+    train_error_score = metrics.r2_score(Ydesert_train,
+            training_desert_prediction)
+    Ydesert_pred = lin_reg_model.predict(Xdesert_test)
+    test_error_score = metrics.r2_score(Ydesert_test, Ydesert_pred)
+    print ("<br/><b>Predicted Desert wastage :", Ydesert_pred)
+
+    
+    
+    
 
 idli_wastage_pred()
 samosa_wastage_pred()
 roti_wastage_pred()
+boiledegg_wastage_pred()
+Chicken_curry_wastage_pred()
+veg_biriyani_wastage_pred()
+veg_meals_wastage_pred()
+desert_wastage_pred()
+
